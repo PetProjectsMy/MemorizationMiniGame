f5ccb90abc91435318ec11d6868a15c888b945b2
Artur Isaev <isaev.artur.1999@mail.ru>
2023-07-17 16:06:07 +0600

    chore: update stylelint config

--- configs/stylelint/stylelint.config.cjs
+++ configs/stylelint/stylelint.config.cjs
@@ -1,18 +1,4 @@
 module.exports = {
-  extends: ['stylelint-config-recommended', 'stylelint-config-clean-order'],
+  extends: ['stylelint-config-clean-order'],
   plugins: ['stylelint-order'],
-  rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['define-placeholder', 'extend', 'define-mixin', 'mixin', 'for'],
-      },
-    ],
-    'function-no-unknown': [
-      true,
-      {
-        ignoreFunctions: ['color-mod', 'blend', 'shade'],
-      },
-    ],
-  },
 };
